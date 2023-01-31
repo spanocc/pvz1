b2427f4e792e179656fcdc86c2344dc86e8a3af3
Adachi <1223784387@qq.com>
2023-02-01 00:25:58 +0800

    feat: implement the function of CreatePlant

--- build/PVZ1_autogen/EWIEGA46WW/moc_pvz_client.cpp
+++ build/PVZ1_autogen/EWIEGA46WW/moc_pvz_client.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_PVZClient_t {
-    QByteArrayData data[1];
-    char stringdata0[10];
+    QByteArrayData data[7];
+    char stringdata0[53];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,17 @@ struct qt_meta_stringdata_PVZClient_t {
     )
 static const qt_meta_stringdata_PVZClient_t qt_meta_stringdata_PVZClient = {
     {
-QT_MOC_LITERAL(0, 0, 9) // "PVZClient"
+QT_MOC_LITERAL(0, 0, 9), // "PVZClient"
+QT_MOC_LITERAL(1, 10, 10), // "CreatPlant"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 4), // "line"
+QT_MOC_LITERAL(4, 27, 6), // "column"
+QT_MOC_LITERAL(5, 34, 10), // "plant_type"
+QT_MOC_LITERAL(6, 45, 7) // "respond"
 
     },
-    "PVZClient"
+    "PVZClient\0CreatPlant\0\0line\0column\0"
+    "plant_type\0respond"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,22 +52,41 @@ static const uint qt_meta_data_PVZClient[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    4,   19,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Bool,    3,    4,    5,    6,
 
        0        // eod
 };
 
 void PVZClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<PVZClient *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->CreatPlant((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< bool(*)>(_a[4]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (PVZClient::*)(int , int , int , bool );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&PVZClient::CreatPlant)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
 }
 
 QT_INIT_METAOBJECT const QMetaObject PVZClient::staticMetaObject = { {
@@ -89,7 +115,25 @@ void *PVZClient::qt_metacast(const char *_clname)
 int PVZClient::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
     return _id;
+}
+
+// SIGNAL 0
+void PVZClient::CreatPlant(int _t1, int _t2, int _t3, bool _t4)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t3))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t4))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
